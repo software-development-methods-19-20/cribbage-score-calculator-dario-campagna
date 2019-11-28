7f0e2b10c09b861adf2afe1c478d9b577102479c
Dario Campagna <campagna@esteco.com>
2019-11-28 12:10:39 +0100

    Extract methods

--- production/source/cribbage/CardParser.java
+++ production/source/cribbage/CardParser.java
@@ -2,10 +2,18 @@ package cribbage;
 
 public class CardParser {
     public static Card parseCard(String cardAsText) {
-        if (cardAsText.charAt(1) == 'H'){
-            return new Card(cardAsText.charAt(0), Suite.HEART);
+        return new Card(parseRank(cardAsText), parseSuite(cardAsText));
+    }
+
+    private static char parseRank(String cardAsText) {
+        return cardAsText.charAt(0);
+    }
+
+    private static Suite parseSuite(String cardAsText) {
+        if (cardAsText.charAt(1) == 'H') {
+            return Suite.HEART;
         } else {
-            return new Card(cardAsText.charAt(0), Suite.DIAMOND);
+            return Suite.DIAMOND;
         }
     }
 }
