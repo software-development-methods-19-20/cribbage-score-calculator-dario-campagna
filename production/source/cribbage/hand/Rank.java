6ec508b0c39c39c42f61ffb3721a1d103fea2dd6
Dario Campagna <campagna@esteco.com>
2019-12-03 13:40:09 +0100

    Make ranks comparable

--- production/source/cribbage/hand/Rank.java
+++ production/source/cribbage/hand/Rank.java
@@ -1,15 +1,37 @@
 package cribbage.hand;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-public class Rank {
+public class Rank implements Comparable {
     private static final Map<Character, Integer> VALUE_BY_CHARACTER = new HashMap<>() {{
         put('A', 1);
+        put('2', 2);
+        put('3', 3);
+        put('4', 4);
+        put('5', 5);
+        put('6', 6);
+        put('7', 7);
+        put('8', 8);
+        put('9', 9);
         put('0', 10);
         put('J', 10);
         put('Q', 10);
         put('K', 10);
+    }};
+    private static final Map<Character, Integer> ORDINAL_BY_CHARACTER = new HashMap<>(){{
+        put('A', 1);
+        put('2', 2);
+        put('3', 3);
+        put('4', 4);
+        put('5', 5);
+        put('6', 6);
+        put('7', 7);
+        put('8', 8);
+        put('9', 9);
+        put('0', 10);
+        put('J', 11);
+        put('Q', 12);
+        put('K', 13);
     }};
     private char rankAsChar;
 
@@ -18,15 +40,17 @@ public class Rank {
     }
 
     public int toInt() {
-        if (VALUE_BY_CHARACTER.containsKey(rankAsChar)) {
-            return VALUE_BY_CHARACTER.get(rankAsChar);
-        } else {
-            return Integer.valueOf("" + rankAsChar);
-        }
+        return VALUE_BY_CHARACTER.get(rankAsChar);
     }
 
     public boolean isJack() {
         return rankAsChar == 'J';
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Rank other = (Rank) o;
+        return ORDINAL_BY_CHARACTER.get(rankAsChar) - ORDINAL_BY_CHARACTER.get(other.rankAsChar);
     }
 
     @Override
--- test/source/test/cribbage/RankTest.java
+++ test/source/test/cribbage/RankTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class RankTest {
 
@@ -36,5 +37,22 @@ public class RankTest {
     @Test
     void kingHasValue10() {
         assertThat(new Rank('K').toInt(), is(10));
+    }
+
+    @Test
+    void aceIsLessThanOtherRanks() {
+        Rank ace = new Rank('A');
+        Rank two = new Rank('2');
+        assertTrue(ace.compareTo(two) < 0);
+        assertTrue(two.compareTo(ace) > 0);
+    }
+
+    @Test
+    void tenIsGreaterThan9AndLessThanJack() {
+        Rank nine = new Rank('9');
+        Rank ten = new Rank('0');
+        Rank jack = new Rank('J');
+        assertTrue(nine.compareTo(ten) < 0);
+        assertTrue(ten.compareTo(jack) < 0);
     }
 }
